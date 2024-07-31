196a896df5c3ae3b1c731d58f76ce65b8603544e
Emmanuel Makae <marnuel19clerk@gmail.com>
2024-07-31 14:50:32 -0700

    c commit

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('registration', views.registration ,name ='registration'),
     path('login', views.login, name='login'),
     path('index', views.index ,name ='index'),
-     path('index1', views.index, name='index')
+    path('index1', views.index, name='index')
 ]
